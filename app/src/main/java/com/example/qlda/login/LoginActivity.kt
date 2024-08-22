05b6e95096b96b44fed791e839843bece5bcd2eb
viet <vietunitydev2222@gmail.com>
2024-08-22 21:43:39 +0700

    - firebase auth logic

--- app/src/main/java/com/example/qlda/login/LoginActivity.kt
+++ app/src/main/java/com/example/qlda/login/LoginActivity.kt
@@ -10,6 +10,10 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.qlda.home.HomeActivity
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+
 
 class LoginActivity : AppCompatActivity() {
 
@@ -18,6 +22,9 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var cbRememberMe: CheckBox
     private lateinit var btnSignIn: Button
     private lateinit var tvNeedHelp: TextView
+
+    // auth
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +37,8 @@ class LoginActivity : AppCompatActivity() {
         btnSignIn = findViewById(R.id.btnSignIn)
         tvNeedHelp = findViewById(R.id.tvNeedHelp)
 
+        auth = Firebase.auth
+
         // Set click listener for Sign In button
         btnSignIn.setOnClickListener {
             val email = etEmail.text.toString()
@@ -37,15 +46,37 @@ class LoginActivity : AppCompatActivity() {
 
             if (email.isEmpty() || password.isEmpty()) {
                 Toast.makeText(this, "Please enter email and password", Toast.LENGTH_SHORT).show()
-            } else {
-                // Perform sign-in logic here (e.g., API call)
+            }
+            else{
+                auth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            // Sign in success, update UI with the signed-in user's information
+                            //Log.d(TAG, "signInWithEmail:success")
+                            val user = auth.currentUser
 
-                Toast.makeText(this, "Signed in as $email", Toast.LENGTH_SHORT).show()
+                            // If successful, we open home activity
+                            Toast.makeText(
+                                baseContext,
+                                "Signed in as $email",
+                                Toast.LENGTH_SHORT).show();
 
-                // If successful, we open home activity
-                 val intent = Intent(this, HomeActivity::class.java)
-                 startActivity(intent)
-                 finish()
+                            val intent = Intent(this, HomeActivity::class.java)
+                            startActivity(intent)
+                            finish()
+
+                            //updateUI(user)
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            //Log.w(TAG, "signInWithEmail:failure", task.exception)
+                            Toast.makeText(
+                                baseContext,
+                                "Authentication failed.",
+                                Toast.LENGTH_SHORT,
+                            ).show()
+                            //updateUI(null)
+                        }
+                    }
             }
         }
 
