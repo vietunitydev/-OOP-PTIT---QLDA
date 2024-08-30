016ff2808f55b395907663297d6084fc2f7a9d4a
viet <vietunitydev2222@gmail.com>
2024-08-30 14:02:07 +0700

    - add more data

--- app/src/main/java/com/example/qlda/home/AppData.java
+++ app/src/main/java/com/example/qlda/home/AppData.java
@@ -24,27 +24,33 @@ public class AppData {
     }
 
     public void fetchData() {
-        Table table = new Table(1, "Project Management", "FF5733");
+        Table table1 = new Table(1, "Project Management", "FF5733");
+        Table table2 = new Table(2, "CTDL", "FF5733");
+        Table table3 = new Table(3, "Meta Rush", "FF5733");
 
-        addTable(table);
+        addTable(table1);
+        addTable(table2);
+        addTable(table3);
 
-        for (int i = 1; i <= 4; i++) {
-            WorkListPage workListPage = new WorkListPage(i, "WorkList " + i);
+        for (Table table : tables) {
+            for (int i = 1; i <= 4; i++) {
+                WorkListPage workListPage = new WorkListPage(i, "WorkList " + i);
 
-            int numberOfElements = (int) (Math.random() * 4) + 3;
+                int numberOfElements = (int) (Math.random() * 4) + 3;
 
-            for (int j = 1; j <= numberOfElements; j++) {
-                Element element = new Element(
-                        j,
-                        "Element " + j,
-                        "Description of Element " + j,
-                        new Date(),
-                        new Date()
-                );
-                workListPage.addElement(element);
+                for (int j = 1; j <= numberOfElements; j++) {
+                    Element element = new Element(
+                            j,
+                            "Element " + j,
+                            "Description of Element " + j,
+                            new Date(),
+                            new Date()
+                    );
+                    workListPage.addElement(element);
+                }
+
+                table.addWorkListPage(workListPage);
             }
-
-            table.addWorkListPage(workListPage);
         }
     }
 }
